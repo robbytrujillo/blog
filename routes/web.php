309bccb84ab86434dffb6356921605a93db6f9f1
robbytrujillo <robbyikusuma88@gmail.com>
2025-08-30 05:31:44 +0700

    Introduction blog

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Back\CategoryController;
 use App\Http\Controllers\Back\DashboardController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,3 +20,5 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', [DashboardController::class, 'index']);
+
+Route::resource('/category', [CategoryController::class]);
