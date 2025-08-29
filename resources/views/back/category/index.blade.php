324d82bdcf42122149c52c6a9502deccaffb3a6b
robbytrujillo <robbyikusuma88@gmail.com>
2025-08-30 05:44:48 +0700

    Introduction blog

---
+++ resources/views/back/category/index.blade.php
@@ -0,0 +1,35 @@
+
+
+@extends('back.layout.template')
+
+@section('content')
+
+    {{--  Content  --}}
+    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Categories</h1>
+      </div>
+
+      <div class="mt-3">
+        <table class="table table-striped">
+            <thead>
+               <tr>
+                 <th>No</th>
+                 <th>Name</th>
+                 <th>Slug</th>
+                 <th>Created At</th>
+                 <th>Function</th>
+               </tr>
+            </thead>
+
+            <tbody>
+                
+            </tbody>
+        </table>
+      </div>
+      
+    </main>
+    
+@endsection
+
+    
--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,4 @@ Route::get('/', function () {
 
 Route::get('/dashboard', [DashboardController::class, 'index']);
 
-Route::resource('/category', [CategoryController::class]);
+Route::resource('/categories', CategoryController::class);
