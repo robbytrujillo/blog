7374cfca38702f339151fff1079a8860c92d1705
robbytrujillo <robbyikusuma88@gmail.com>
2025-08-31 06:24:23 +0700

    Introduction blog

--- resources/views/back/category/create-modal.blade.php
+++ resources/views/back/category/create-modal.blade.php
@@ -1,18 +1,27 @@
 <!-- Modal -->
 <div class="modal fade" id="modalCreate" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-<div class="modal-dialog rounded-pill">
-<div class="modal-content">
-    <div class="modal-header bg-light">
-    <h1 class="modal-title fs-5" id="staticBackdropLabel">Create Categories</h1>
-    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    <div class="modal-dialog rounded-pill">
+        <div class="modal-content">
+            <div class="modal-header bg-light">
+                <h1 class="modal-title fs-5" id="staticBackdropLabel">Create Categories</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+
+            <div class="modal-body">
+                <form action="" method="post">
+                    @csrf
+
+                    <div class="mb-3">
+                        <label for="name">Name</label>
+                        <input type="text" name="name" id="name" class="form-control">
+                    </div>
+
+                     <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary rounded-pill" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary rounded-pill">Save</button>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
-    <div class="modal-body">
-    ...
-    </div>
-    <div class="modal-footer">
-    <button type="button" class="btn btn-secondary rounded-pill" data-bs-dismiss="modal">Close</button>
-    <button type="button" class="btn btn-primary rounded-pill">Understood</button>
-    </div>
-</div>
-</div>
 </div>
