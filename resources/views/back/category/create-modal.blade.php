407876089137742b5b1bbd22244f1abec677afbc
robbytrujillo <robbyikusuma88@gmail.com>
2025-08-31 06:14:51 +0700

    Introduction blog

--- resources/views/back/category/create-modal.blade.php
+++ resources/views/back/category/create-modal.blade.php
@@ -1,8 +1,8 @@
 <!-- Modal -->
 <div class="modal fade" id="modalCreate" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-<div class="modal-dialog">
+<div class="modal-dialog rounded-pill">
 <div class="modal-content">
-    <div class="modal-header">
+    <div class="modal-header bg-light">
     <h1 class="modal-title fs-5" id="staticBackdropLabel">Create Categories</h1>
     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
     </div>
