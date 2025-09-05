3a38f031a01991b33a38147489c7c205ce8f0ee1
robbytrujillo <robbyikusuma88@gmail.com>
2025-09-05 11:17:06 +0700

    Introduction blog | Articles Page

--- resources/views/back/article/index.blade.php
+++ resources/views/back/article/index.blade.php
@@ -1,5 +1,9 @@
 @extends('back.layout.template')
 
+@push('css')
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.3.3/css/dataTables.bootstrap5.css">
+@endpush
+
 @section('content')
 
     {{--  Content  --}}
@@ -35,7 +39,7 @@
           @endif
 
         <table class="table table-striped table-bordered">
-            <thead>
+            <thead class="text-center">
                <tr>
                  <th>No</th>
                  <th>Title</th>
@@ -71,5 +75,12 @@
     </main>
     
 @endsection
+
+@push('js')
+    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.datatables.net/2.3.3/js/dataTables.js"></script>
+    <script src="https://cdn.datatables.net/2.3.3/js/dataTables.bootstrap5.js"></script>
+@endpush
 
     
--- resources/views/back/layout/template.blade.php
+++ resources/views/back/layout/template.blade.php
@@ -29,6 +29,9 @@
 
     <!-- Custom styles for this template -->
     <link href="{{ asset('back/css/dashboard.css') }}" rel="stylesheet">
+
+    {{--  Dinamic CSS Calling Per-page  --}}
+    @stack('css')
   </head>
   <body>
     
@@ -64,5 +67,7 @@
 
       <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script>
       <script src={{ asset('back/js/dashboard.js') }}></script>
+
+      @stack('js')
   </body>
 </html>
